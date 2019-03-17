63ac5ad417cbe256cb0a44eafbde1030ea045ee4
The-Hierophant <45378227+The-Hierophant@users.noreply.github.com>
2019-03-17 23:21:58 +0800

    version 0.1 add "camup" "camdown"

--- PiBotServer/bin/direct.py
+++ PiBotServer/bin/direct.py
@@ -5,4 +5,5 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(19, GPIO.OUT)
 pwm=GPIO.PWM(19,50)
-pwm.start(float(sys.argv[1])
+pwm.start(float(sys.argv[1]))
+time.sleep(1)
--- PiBotServer/bin/direct_2.py
+++ PiBotServer/bin/direct_2.py
@@ -5,4 +5,5 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(26, GPIO.OUT)
 pwm=GPIO.PWM(26,50)
-pwm.start(float(sys.argv[1])
+pwm.start(float(sys.argv[1]))
+time.sleep(1)
